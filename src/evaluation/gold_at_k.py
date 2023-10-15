77938b3d9aee380ce0f1ddc4746e8751c7eabb04
Yikang Pan <123642674+MexicanLemonade@users.noreply.github.com>
2023-10-14 20:07:28 -0400

    Update helpers

--- src/evaluation/gold_at_k.py
+++ src/evaluation/gold_at_k.py
@@ -30,19 +30,3 @@ def gen_at_k(result_file, k=100):
             if title.startswith('<G>'):
                 correct += 1
     return correct/total
-
-if __name__ == '__main__':
-    query_file = '/mnt/victoria/data3/liangming/yikangpan/nela-gt-main/question-generation/attempt4-large/politics/reliable/qas/politics-QG2-num-203-T0.json'
-    result_file = '/mnt/victoria/data3/liangming/yikangpan/nela-gt-main/embeddings-DPR/definitive-large/politics/DPR-malicious-1psg/output.json'
-    print("Gold@k scores: ")
-    print("k=1 ", gold_at_k(result_file, query_file, 1))
-    print("k=5, ", gold_at_k(result_file, query_file, 5))
-    print("k=10, ", gold_at_k(result_file, query_file, 10))
-    print("k=20, ", gold_at_k(result_file, query_file, 20))
-    print("k=100, ", gold_at_k(result_file, query_file, 100))
-    print("Gen@k scores:")
-    print("k=1, " , gen_at_k(result_file, 1))
-    print("k=5, " , gen_at_k(result_file, 5))
-    print("k=10, " , gen_at_k(result_file, 10))
-    print("k=20, " , gen_at_k(result_file, 20))
-    print("k=100, " , gen_at_k(result_file, 100))
